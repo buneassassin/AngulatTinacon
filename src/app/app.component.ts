e523b096f98dadc97b5fc11e27d820a3a13a6bc1
buneassassin <bune_assassin@hotmail.com>
2025-02-17 16:11:07 -0600

    mas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './Components/nav/nav.component'; // 🚀 Importa el Navbar
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ NavComponent], // ✅ Importa los componentes aquí
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
