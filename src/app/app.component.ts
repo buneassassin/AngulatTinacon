2527e5bcdbbd29a427dc39c5feef881c6ec2ad24
buneassassin <bune_assassin@hotmail.com>
2025-02-19 11:00:12 -0600

    Add conditional rendering for navbar and footer based on authentication page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Route } from '@angular/router';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { NavComponent } from './Components/nav/nav.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule,FooterComponent,NavComponent], // Importa RouterOutlet aquí
+  imports: [RouterModule,FooterComponent,NavComponent,CommonModule], // Importa RouterOutlet aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'IntegradoraAngularTinacon';
+  isAuthPage = false; 
+
+  constructor(private router: Router) {
+    // Detectar cambios en la ruta
+    this.router.events.subscribe(() => {
+      const authRoutes = ['/login', '/register']; // Rutas donde NO quieres mostrar el navbar y footer
+      this.isAuthPage = authRoutes.includes(this.router.url);
+    });
+  }
 }
