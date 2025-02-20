59ff9cb8e84fda24c9d17173a222505614ecc210
“TorresTG” <totilotegabriel@gmail.com>
2025-02-20 10:05:06 -0600

    instrucciones de uso para el modal

--- src/app/Components/modal/modal.component.ts
+++ src/app/Components/modal/modal.component.ts
@@ -11,4 +11,25 @@ export class ModalComponent {
   @Input() title: string = ''; // Título dinámico
   @Output() save = new EventEmitter<void>(); // Evento para el botón guardar
   @Output() close = new EventEmitter<void>(); // Evento para cerrar el modal
+
+  /*
+  para utilizar el modal se debe de poner los siguientes metodos para que el modal funcione correctamente (no pense en que 
+  el metodo save guardara realmente los datos y NO actua como un boton submit):
+
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  saveModal() {
+    console.log('Guardado con éxito');
+    this.closeModal();
+  }
+  
+  */
 }
