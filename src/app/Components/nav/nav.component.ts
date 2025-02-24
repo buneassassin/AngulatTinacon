e3a4fd134da1d9c7d78ad4e4b91524351a5e06ce
buneassassin <bune_assassin@hotmail.com>
2025-02-24 17:00:26 -0600

    refactor navbar component; implement login/register buttons and user menu based on authentication state

--- src/app/Components/nav/nav.component.ts
+++ src/app/Components/nav/nav.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterLink,RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Usa CommonModule en lugar de BrowserModule
+
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink,RouterLinkActive],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
+  ngOnInit(): void {
+    // Actualiza la variable según la existencia del token
+    this.isLoggedIn = !!localStorage.getItem('token');
+  }
 }
