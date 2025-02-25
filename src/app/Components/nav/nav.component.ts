8b3d8670470b194f8cc0802e89a45497f4be1749
buneassassin <bune_assassin@hotmail.com>
2025-02-24 18:44:14 -0600

    fetch user data and display profile picture in navbar for logged-in users

--- src/app/Components/nav/nav.component.ts
+++ src/app/Components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../Services/auth/auth.service';
+import { User } from '../../Interface/user';
 
 @Component({
   selector: 'app-nav',
@@ -12,13 +13,24 @@ import { AuthService } from '../../Services/auth/auth.service';
 })
 export class NavComponent implements OnInit {
   isLoggedIn: boolean = false;
+  user: User | null = null; // Aquí se almacenará la información del usuario
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.isLoggedIn = !!localStorage.getItem('token');
+    this.info();
   }
-
+  info(): void {
+    this.authService.getUserData().subscribe({
+      next: (response: any) => {
+        if (response.success) {
+          this.user = response.user;
+        }
+      },
+      error: (error) => console.error('Error al obtener datos del usuario', error)
+    });
+  }
   logout(): void {
     this.authService.logoutUser().subscribe({
       next: (response) => {
